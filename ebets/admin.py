3bc00fe4464f3271a0b2561511890ef22630cf34
Peter Horvath <horvthpeter@gmail.com>
2014-06-06 12:38:40 +0000

    Added models to admin.

--- ebets/admin.py
+++ ebets/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from ebets.models import Player, Match, Team, Event
 
-# Register your models here.
+
+class PlayerAdmin(admin.ModelAdmin):
+    fields = ('nickname', 'team', 'first_name', 'last_name',
+              'pic', 'description')
+
+
+admin.site.register(Player, PlayerAdmin)
+
+
+class TeamAdmin(admin.ModelAdmin):
+    fields = ('name', 'description', 'logo')
+
+
+admin.site.register(Team, TeamAdmin)
+
+
+class MatchAdmin(admin.ModelAdmin):
+    fields = ('radiant', 'dire', 'date', 'event', 'description', 'winner',
+              'over')
+
+
+admin.site.register(Match, MatchAdmin)
+
+
+class EventAdmin(admin.ModelAdmin):
+    fields = ('name', 'logo', 'description', 'online', 'offline')
+
+
+admin.site.register(Event, EventAdmin)
